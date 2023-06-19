f6f7ff02ca4b6ae13095903d85ab55de110d31fd
uaravindshenoy <75222342+uaravindshenoy@users.noreply.github.com>
2023-06-19 17:56:18 +0530

    event search functionality implemented

--- components/events/events-search.tsx
+++ components/events/events-search.tsx
@@ -1,20 +1,32 @@
+import { FormEvent, useRef } from "react";
+
 import Button from "../ui/button";
 import classes from "./events-search.module.css";
 
-function EventsSearch() {
+function EventsSearch(props: {onSearch: (selectedYear:string,selectedMonth:string)=>void}) {
+    const yearInputRef = useRef<HTMLSelectElement>(null);
+    const monthInputRef = useRef<HTMLSelectElement>(null);
+    function submitHandler(event:FormEvent<HTMLFormElement>){
+        event.preventDefault();
+
+        const selectedYear = yearInputRef.current?.value || '';
+        const selectedMonth = monthInputRef.current?.value || '';
+
+        props.onSearch(selectedYear,selectedMonth);
+    }
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <div className={classes.controls}>
         <div className={classes.control}>
           <label htmlFor="year">Year</label>
-          <select id="year">
+          <select id="year" ref={yearInputRef}>
             <option value="2021">2021</option>
             <option value="2022">2022</option>
           </select>
         </div>
         <div className={classes.control}>
           <label htmlFor="month">Month</label>
-          <select id="month">
+          <select id="month" ref={monthInputRef}>
             <option value="1">January</option>
             <option value="2">February</option>
             <option value="3">March</option>
