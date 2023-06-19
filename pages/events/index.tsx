5d52005b616fc6d108c154c2c7722009f4e2a11e
uaravindshenoy <75222342+uaravindshenoy@users.noreply.github.com>
2023-06-19 15:06:26 +0530

    all events page populated using EventList component

--- pages/events/index.tsx
+++ pages/events/index.tsx
@@ -1,9 +1,13 @@
-function Events() {
+import { getAllEvents } from "@/dummy-data";
+import EventList from "@/components/events/event-list";
+
+function AllEventsPage() {
+    const events = getAllEvents();
   return (
     <div>
-      <h1>All events</h1>
+      <EventList items={events} />
     </div>
   );
 }
 
-export default Events;
+export default AllEventsPage;
